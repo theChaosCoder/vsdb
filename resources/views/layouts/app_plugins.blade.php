1fcef04dbee2be17057df3217b7b2f3e911a2874
Unknown <thechaoscoder@gmail.com>
2020-08-30 19:43:51 +0200

    Add category select to table

--- resources/views/layouts/app_plugins.blade.php
+++ resources/views/layouts/app_plugins.blade.php
@@ -81,7 +81,27 @@
          			return group +' ('+rows.count()+')';
          		},
          		dataSrc: 5
-         	},
+             },
+             initComplete: function () {
+                    this.api().columns([5]).every( function () {
+                        var column = this;
+                        var select = $('<select><option value=""></option></select>')
+                            .appendTo( $(column.header()).empty() )
+                            .on( 'change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                    $(this).val().replace(/(<([^>]+)>)/ig,"")
+                                );
+
+                                column
+                                    .search( val ? '^'+val+'$' : '', true, false )
+                                    .draw();
+                            } );
+
+                        column.data().unique().sort().each( function ( d, j ) {
+                            select.append( '<option value="'+d+'">'+d+'</option>' )
+                        } );
+                    } );
+                },
 			"order": [[ 1, "asc" ]]
          });
 
@@ -124,7 +144,8 @@
 					//console.log(gpu);
 					return false;
 				}
-			);
+            );
+
 			// Event listener checkbox
 				$('#gpu').change( function() {
 					table.draw();
