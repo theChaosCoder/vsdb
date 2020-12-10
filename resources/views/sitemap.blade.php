7448413aee3fde2e22892f03a2e3922a765cc1e2
Unknown <thechaoscoder@gmail.com>
2020-12-10 15:19:19 +0100

    Use getSchemeAndHttpHost() in sitemap generator

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($plugins as $plugin)
         <url>
-            <loc>http://vsdb.top/plugins/{{ $plugin->namespace }}</loc>
+            <loc>{{ request()->getSchemeAndHttpHost() }}/plugins/{{ $plugin->namespace }}</loc>
             <lastmod>{{ $plugin->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.6</priority>
